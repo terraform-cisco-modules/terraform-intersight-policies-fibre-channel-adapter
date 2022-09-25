e06cb6284e3c1b6fc96461d097d845b48f384bdb
scotttyso <tyscott@cisco.com>
2022-09-25 01:31:59 -0400

    new TF 1.3 and organization moid option

--- examples/complete/README.md
+++ examples/complete/README.md
@@ -1,5 +1,5 @@
 <!-- BEGIN_TF_DOCS -->
-# Fibre-Channel Pool Example
+# Fibre Channel Adapter Policy Example
 
 To run this example you need to execute:
 
@@ -13,23 +13,28 @@ Note that this example will create resources. Resources can be destroyed with `t
 
 ### main.tf
 ```hcl
-module "wwpn_pool" {
-  source  = "scotttyso/pools-fc/intersight"
+module "fibre_channel_adapter" {
+  source  = "terraform-cisco-modules/policies-fibre-channel-adapter/intersight"
   version = ">= 1.0.1"
 
-  assignment_order = "sequential"
-  description      = "Demo WWPN Pool"
-  id_blocks = [
-    {
-      from = "0:00:00:25:B5:00:00:00"
-      size = 1000
-    }
-  ]
-  name         = "default"
-  organization = "default"
-  pool_purpose = "WWPN"
+  adapter_template = "WindowsBoot"
+  description      = "default Fibre Channel Adapter Adapter Policy."
+  name             = "default"
+  organization     = "default"
 }
+```
 
+### provider.tf
+```hcl
+terraform {
+  required_providers {
+    intersight = {
+      source  = "CiscoDevNet/intersight"
+      version = ">=1.0.32"
+    }
+  }
+  required_version = ">=1.3.0"
+}
 ```
 
 ### variables.tf
@@ -50,24 +55,6 @@ variable "secretkey" {
   description = "Intersight Secret Key."
   sensitive   = true
   type        = string
-}
-```
-
-### versions.tf
-```hcl
-terraform {
-  required_providers {
-    intersight = {
-      source  = "CiscoDevNet/intersight"
-      version = ">=1.0.32"
-    }
-  }
-}
-
-provider "intersight" {
-  apikey    = var.apikey
-  endpoint  = var.endpoint
-  secretkey = var.secretkey
 }
 ```
 <!-- END_TF_DOCS -->
--- main.tf
+++ main.tf
@@ -5,7 +5,12 @@
 #____________________________________________________________
 
 data "intersight_organization_organization" "org_moid" {
-  name = var.organization
+  for_each = {
+    for v in [var.organization] : v => v if length(
+      regexall("[[:xdigit:]]{24}", var.organization)
+    ) == 0
+  }
+  name = each.value
 }
 
 #__________________________________________________________________
@@ -18,16 +23,16 @@ resource "intersight_vnic_fc_adapter_policy" "fibre_channel_adapter" {
   depends_on = [
     data.intersight_organization_organization.org_moid
   ]
-  description                 = length(
-        regexall("(FCNVMeInitiator)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for FCNVMeInitiator." : length(
-        regexall("(FCNVMeTarget)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for FCNVMeTarget." : length(
-        regexall("(Initiator)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for Initiator." : length(
-        regexall("(Linux)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for linux." : length(
-        regexall("(Solaris)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for Solaris." : length(
-        regexall("(VMware)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for VMware." : length(
-        regexall("(WindowsBoot)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for WindowsBoot." : length(
-        regexall("(Windows)", coalesce(var.adapter_template, "EMPTY"))
-      ) > 0 ? "Recommended adapter settings for Windows." : var.description != "" ? var.description : "${var.name} Fibre-Channel Adapter Policy."
+  description = length(
+    regexall("(FCNVMeInitiator)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for FCNVMeInitiator." : length(
+    regexall("(FCNVMeTarget)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for FCNVMeTarget." : length(
+    regexall("(Initiator)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for Initiator." : length(
+    regexall("(Linux)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for linux." : length(
+    regexall("(Solaris)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for Solaris." : length(
+    regexall("(VMware)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for VMware." : length(
+    regexall("(WindowsBoot)", coalesce(var.adapter_template, "EMPTY"))) > 0 ? "Recommended adapter settings for WindowsBoot." : length(
+    regexall("(Windows)", coalesce(var.adapter_template, "EMPTY"))
+  ) > 0 ? "Recommended adapter settings for Windows." : var.description != "" ? var.description : "${var.name} Fibre-Channel Adapter Policy."
   error_detection_timeout     = var.error_detection_timeout
   io_throttle_count           = var.io_throttle_count
   lun_count                   = var.max_luns_per_target
@@ -40,8 +45,8 @@ resource "intersight_vnic_fc_adapter_policy" "fibre_channel_adapter" {
     io_retry_timeout  = var.error_recovery_io_retry_timeout
     link_down_timeout = var.error_recovery_link_down_timeout
     port_down_timeout = var.adapter_template == "WindowsBoot" ? 5000 : length(
-        regexall("(FCNVMeInitiator|Initiator|Solaris|VMware|Windows)", coalesce(var.adapter_template, "EMPTY"))
-      ) > 0 ? 30000 : var.error_recovery_port_down_timeout
+      regexall("(FCNVMeInitiator|Initiator|Solaris|VMware|Windows)", coalesce(var.adapter_template, "EMPTY"))
+    ) > 0 ? 30000 : var.error_recovery_port_down_timeout
   }
   flogi_settings {
     retries = var.flogi_retries
@@ -51,7 +56,11 @@ resource "intersight_vnic_fc_adapter_policy" "fibre_channel_adapter" {
     mode = var.interrupt_mode
   }
   organization {
-    moid        = data.intersight_organization_organization.org_moid.results[0].moid
+    moid = length(
+      regexall("[[:xdigit:]]{24}", var.organization)
+      ) > 0 ? var.organization : data.intersight_organization_organization.org_moid[
+      var.organization].results[0
+    ].moid
     object_type = "organization.Organization"
   }
   plogi_settings {
@@ -59,19 +68,17 @@ resource "intersight_vnic_fc_adapter_policy" "fibre_channel_adapter" {
     timeout = var.adapter_template == "WindowsBoot" ? 4000 : var.plogi_timeout
   }
   rx_queue_settings {
-    nr_count  = 1
     ring_size = length(
-        regexall("(FCNVMeTarget|Target)", coalesce(var.adapter_template, "EMPTY"))
-      ) > 0 ? 2048 : var.receive_ring_size
+      regexall("(FCNVMeTarget|Target)", coalesce(var.adapter_template, "EMPTY"))
+    ) > 0 ? 2048 : var.receive_ring_size
   }
   scsi_queue_settings {
-    nr_count  = length(
-        regexall("(FCNVMeTarget|FCNVMeInitiator)", coalesce(var.adapter_template, "EMPTY"))
-      ) > 0 ? 16 : var.scsi_io_queue_count
+    nr_count = length(
+      regexall("(FCNVMeTarget|FCNVMeInitiator)", coalesce(var.adapter_template, "EMPTY"))
+    ) > 0 ? 16 : var.scsi_io_queue_count
     ring_size = var.scsi_io_ring_size
   }
   tx_queue_settings {
-    nr_count  = 1
     ring_size = var.transmit_ring_size
   }
   dynamic "tags" {
